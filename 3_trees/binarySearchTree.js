e93375c1537ccd58d934c3bf535e7108b48a6a5f
wizard-jS <spraguej624@gmail.com>
2019-04-23 08:26:22 -0700

    Add insert and find methods

--- 3_trees/binarySearchTree.js
+++ 3_trees/binarySearchTree.js
@@ -9,8 +9,45 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value, node = this.root) {
+    var newNode = new Node(value);
+
+    if (!node) {
+      this.root = newNode;
+    } else if (newNode.value > node.value) {
+      if (!node.right) {
+        node.right = newNode;
+      } else {
+        this.insert(value, node.right);
+      }
+    } else {
+      if (!node.left) {
+        node.left = newNode;
+      } else {
+        this.insert(value, node.left);
+      }
+    }
+    return this.root;
+  }
+
+  find(value, node = this.root) {
+    if (value === node.value) {
+      return true;
+    } else if (value > node.value) {
+      return !!(node.right && this.find(value, node.right));
+    } else if (value < node.value) {
+      return !!(node.left && this.find(value, node.left));
+    }
+  }
 }
 
 var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(14);
+tree.insert(8);
+tree.insert(4);
+tree.insert(19);
+console.log(tree.find(1));
 
 // console.log(tree);
