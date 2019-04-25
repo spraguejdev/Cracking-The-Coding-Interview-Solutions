92cadf04fc6b228fa8ec1580777ad43877a95a92
wizard-jS <spraguej624@gmail.com>
2019-04-25 16:54:56 -0700

    Export BST

--- 3_trees/binarySearchTree.js
+++ 3_trees/binarySearchTree.js
@@ -5,7 +5,7 @@ class Node {
     this.right = null;
   }
 }
-class BinarySearchTree {
+module.exports = class BinarySearchTree {
   constructor() {
     this.root = null;
   }
@@ -93,14 +93,14 @@ class BinarySearchTree {
     }
     return data;
   }
-}
+};
 
-var tree = new BinarySearchTree();
-tree.insert(10);
-tree.insert(6);
-tree.insert(8);
-tree.insert(3);
-tree.insert(15);
-tree.insert(20);
-console.log(tree.dfsInOrder());
-// console.log(tree);
+// var tree = new BinarySearchTree();
+// tree.insert(10);
+// tree.insert(6);
+// tree.insert(8);
+// tree.insert(3);
+// tree.insert(15);
+// tree.insert(20);
+// console.log(tree.dfsInOrder());
+// // console.log(tree);
