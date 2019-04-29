7833623ffeb83de1e57b07b6ac91f125f01eac8d
wizard-jS <spraguej624@gmail.com>
2019-04-28 19:09:46 -0700

    House Keeping

--- leetcode/threeSum.js
+++ leetcode/threeSum.js
@@ -38,6 +38,3 @@ var threeSum = function(nums) {
 
 console.log(threeSum([-1, 0, 1, 2, -1, -4]));
 console.log(threeSum([0, 0, 0, 0]));
-
-console.log(threeSum([-1, 0, 1, 2, -1, -4]));
-console.log(threeSum([0, 0, 0, 0]));
