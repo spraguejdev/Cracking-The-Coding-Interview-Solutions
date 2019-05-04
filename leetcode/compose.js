e0fbaabf7ad7e0fe867df865d1e64920663fcced
wizard-jS <spraguej624@gmail.com>
2019-05-04 11:27:35 -0700

    Add compose.js

---
+++ leetcode/compose.js
@@ -0,0 +1,29 @@
+var add1 = val => val + 1;
+var multipleBy5 = val => val * 5;
+
+var compose = (...fns) => val => fns.reduceRight((memo, fn) => fn(memo), val);
+
+var composed = compose(
+  add1,
+  multipleBy5
+);
+
+console.log(composed(5));
+
+// Another way to write compose
+
+var compose = function(add, multiple) {
+  var fns = arguments;
+
+  // var fns = Array.prototype.slice.call(arguments);
+  return function(val) {
+    for (let i = fns.length - 1; i >= 0; i--) {
+      val = fns[i](val);
+    }
+    return val;
+  };
+};
+
+var composed2 = compose(multipleBy5);
+
+console.log(composed2(5));
