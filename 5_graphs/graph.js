00e8308ce99f560d8e42e2a30096534968c596d6
wizard-jS <spraguej624@gmail.com>
2019-05-12 10:49:07 -0700

    Add BFS + DFS

--- 5_graphs/graph.js
+++ 5_graphs/graph.js
@@ -21,7 +21,7 @@ class Graph {
 
   removeEdge(vertex1, vertex2) {
     this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(vertex => {
-      return vertex !== vertex2;
+      return vertex !== vaertex2;
     });
     this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(vertex => {
       return vertex !== vertex1;
@@ -35,4 +35,95 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+  dfsRecurse(start) {
+    var visited = {};
+    var results = [];
+    const adjacencyList = this.adjacencyList;
+    (function dfs(vertex) {
+      if (!vertex) {
+        return null;
+      }
+      visited[vertex] = true;
+      results.push(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start);
+    return results;
+  }
+  dfsIterative(start) {
+    var visited = {};
+    var results = [];
+    var stack = [];
+    stack.push(start);
+    while (stack.length) {
+      var temp = stack.pop();
+      if (!visited[temp]) {
+        visited[temp] = true;
+        results.push(temp);
+        this.adjacencyList[temp].forEach(neighbor => {
+          stack.push(neighbor);
+        });
+      }
+    }
+    return results;
+  }
+
+  dfsIterative(start) {
+    var visited = {};
+    var results = [];
+    var stack = [];
+    var temp;
+    stack.push(start);
+    visited[start] = true;
+    while (stack.length) {
+      temp = stack.pop();
+      results.push(temp);
+      this.adjacencyList[temp].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return results;
+  }
+
+  bfsIterative(start) {
+    var visited = {};
+    var queue = [start];
+    var results = [];
+    var temp;
+    visited[start] = true;
+
+    while (queue.length) {
+      temp = queue.shift();
+      results.push(temp);
+      this.adjacencyList[temp].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return results;
+  }
 }
+
+var graph = new Graph();
+graph.addVertex(3);
+graph.addVertex(4);
+graph.addVertex(5);
+graph.addVertex(6);
+graph.addVertex(7);
+graph.addVertex(8);
+graph.addEdge(3, 6);
+graph.addEdge(5, 8);
+graph.addEdge(4, 5);
+graph.addEdge(4, 6);
+graph.addEdge(4, 7);
+graph.addEdge(5, 6);
+graph.addEdge(6, 7);
+console.log(graph.bfsIterative(4));
