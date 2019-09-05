294fa71ddba914dd4718205e46c44251e316f0f1
wizard-jS <spraguej624@gmail.com>
2019-09-05 05:45:06 -0700

    Add twoSum.js

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -3,24 +3,24 @@ var twoSum = function(nums, target) {
   const len = nums.length;
   let map = {};
   for (let i = 0; i < len; i++) {
-    let n = target - nums[i];
-    let find = map[n];
+    let n = target - nums[i]; //
+    let find = map[n]; // 11
     if (find !== undefined) return [find, i];
     else map[nums[i]] = i;
   }
 };
 
-// function twoSum(array, target) {
-//   const numsMap = new Map();
-//   for (let i = 0; i < array.length; i++) {
-//       if(numsMap.has(target - array[i])) {
-//           return [numsMap.get(target - array[i], i)];
-//           // get() returns a specified element associated with the specified key from the Map object.
-//       } else {
-//           numsMap.set(array[i], i);
-//           //  set() adds or updates an element with a specified key and value to a Map object.
-//       }
-//   }
-// }
+function twoSum(array, target) {
+  const numsMap = new Map();
+  for (let i = 0; i < array.length; i++) {
+    if (numsMap.has(target - array[i])) {
+      return [numsMap.get(target - array[i], i)];
+      // get() returns a specified element associated with the specified key from the Map object.
+    } else {
+      numsMap.set(array[i], i);
+      //  set() adds or updates an element with a specified key and value to a Map object.
+    }
+  }
+}
 
 console.log(twoSum([1, 4, 6, 7, 8, 6], 12));
